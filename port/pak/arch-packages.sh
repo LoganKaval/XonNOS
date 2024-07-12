795c70c64b23e5e5ba1008d5dc3041defb5c6fa0
Logan Kaval <136924755+LoganKaval@users.noreply.github.com>
2024-07-12 10:43:33 -0500

    [ 4.Simpler-Installer]/Not-Needed

--- port/pak/arch-packages.sh
+++ port/pak/arch-packages.sh
@@ -2,36 +2,36 @@
 
 sudo pacman -Syu
 sudo pacman -Syy
-sudo pacman -S tty-clock -y
-sudo pacman -S chromium-browser -y
-sudo pacman -S thunar -y
-sudo pacman -S libreoffice -y
-sudo pacman -S cheese -y
-sudo pacman -S git -y
-sudo pacman -S wget -y
-sudo pacman -S river -Y
-sudo pacman -S waybar
-sudo pacman -S foot
-sudo pacman -S fuzzel
-sudo pacman -S swaybg
-sudo pacman -S terminator -y
-sudo pacman -S featherpad -y
-sudo pacman -S vim -y
-sudo pacman -S screenfetch -y
-sudo pacman -S ranger -y
-sudo pacman -S btop -y
-sudo pacman -S lxappearance -y
-sudo pacman -S qt5ct -y
-sudo pacman -S gnome-clocks -y
-sudo pacman -S xfce4-power-manager -y
-sudo pacman -S nano -y
-sudo pacman -S mc -y
-sudo pacman -S curl -y
+sudo pacman -S tty-clock \
+chromium-browser \
+thunar \
+libreoffice \
+cheese \
+git \
+wget \
+river \
+waybar \
+foot \
+fuzzel \
+swaybg \
+terminator \
+featherpad \
+vim \
+screenfetch \
+ranger \
+btop \
+lxappearance \
+qt5ct \
+gnome-clocks \
+xfce4-power-manager \
+nano \
+mc \
+curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo pacman -Syu
-sudo pacman -S spectacle -y
-sudo pacman -S ufw -y
+sudo pacman -S spectacle \
+ufw \
 
 echo "[     1.41H_4.14u                ] Finished: 'arch-packages.sh'  script--"
 echo "[     3.13A_1.41l_4.14l_1.9a_7.0h] Continuing: 'arch-to-jaiyk.sh' script--"
